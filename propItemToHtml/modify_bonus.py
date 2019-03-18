6b719db54ad73079abd4803c3dd7b82f21147d90
SquonK <sp@squonk.fr>
2019-03-18 02:54:54 +0100

    Update modify bonus

--- propItemToHtml/modify_bonus.py
+++ propItemToHtml/modify_bonus.py
@@ -6,16 +6,12 @@ from jinja2 import Environment, FileSystemLoader
 import os
 import items_manager
 
-path = "..\\..\\FlyFF-VS17\\Resource\\" if len(sys.argv) < 2 else sys.argv[1]
-propItem_filename = "propItem.txt" if len(sys.argv) < 3 else sys.argv[2]
-number_of_parameters = 6 if len(sys.argv) < 4 else sys.argv[3]
-
-item_manager = items_manager.get_item_manager(number_of_parameters)
-item_list = items_manager.get_item_list(path + propItem_filename)
+item_manager = items_manager.get_item_manager()
+item_list = items_manager.get_item_list()
 
 modified_items = {}
 
-with open("changed_bonus.txt") as f:
+with open(items_manager.THIS_DIR + "changed_bonus.txt") as f:
     for line in f.readlines():
         line = line.strip()
         if line is None or len(line) == 0:
@@ -28,7 +24,7 @@ with open("changed_bonus.txt") as f:
         new_bonus = []
         
         
-        for i in range(number_of_parameters):
+        for i in range(items_manager.nb_param()):
             bonus_type = splitted[1 + i * 2]
             bonus_value = splitted[1 + i * 2 + 1]
             
@@ -37,7 +33,7 @@ with open("changed_bonus.txt") as f:
             
             new_bonus.append((bonus_type, bonus_value))
         
-        while len(new_bonus) != number_of_parameters:
+        while len(new_bonus) != items_manager.nb_param():
             new_bonus.append(("=", "="))
         
         modified_items[id] = new_bonus
@@ -46,7 +42,7 @@ with open("changed_bonus.txt") as f:
 
 new_propItemContent = []
 
-with open(path + propItem_filename, encoding="ansi") as f:
+with open(items_manager.getPropItemPath(), encoding="ansi") as f:
     for line in f.readlines():    
         origline = line
         line = line.replace(str(chr(10)), "").replace("\r", "").strip()
@@ -72,13 +68,13 @@ with open(path + propItem_filename, encoding="ansi") as f:
         id = parameters_list[item_manager['ID']]
         rewritten_bonus = modified_items[id]
         
-        for i in range(number_of_parameters):
+        for i in range(items_manager.nb_param()):
             parameters_list[i + item_manager['START_DW_PARAM']] = rewritten_bonus[i][0]
             parameters_list[i + item_manager['START_ADJ_PARAM']] = rewritten_bonus[i][1]
         
         new_propItemContent.append("\t".join(parameters_list))
 
 
-f = open(path + propItem_filename, "w+", encoding="ansi")
+f = open(items_manager.modifiedPropItem(), "w+", encoding="ansi")
 f.write("\n".join(new_propItemContent))
 f.close()
